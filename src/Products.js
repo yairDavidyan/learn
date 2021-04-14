8b74da9b19f43d43978e4871acd2ebab3b57ea58
Yair <yair22218@gmail.com>
2021-04-14 13:25:39 +0300

    commit first

---
+++ src/Products.js
@@ -0,0 +1,19 @@
+import './products.css';
+
+function Products(props) {
+    const { items } = props;
+    
+    
+
+    return (
+        <div className ="div-products">
+            <div className="container=image">                
+                <img src={items.image} className="image"></img>
+                <div className="title">{ items.title}</div>
+            </div>
+            <div className="description">{ items.description}</div>
+            <div className="price">{items.price}</div>
+        </div>
+    );
+}
+export default Products;
